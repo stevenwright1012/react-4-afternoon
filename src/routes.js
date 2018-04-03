1c84a65659a8a7cd2d21f0641762cf4221e056a4
Steven <stevenwright1012@gmail.com>
2018-04-03 15:33:09 -0600

    finished Black Diamond

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Switch} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
+import Home from './components/Home/Home.js';
+import About from './components/About/About.js';
+import ClassList from './components/ClassList/ClassList'
+import Student from './components/Student/Student'
+
+
+export default(
+    <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/about' component={About}/>
+        <Route path='/classlist/:class' component={ClassList}/>
+        <Route path='/student/:id' component={Student}/>
+    </Switch>
+)
